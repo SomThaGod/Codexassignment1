40d711235ff8209156e693733ba3774fe77b71d5
SomThaGod <53866232+SomThaGod@users.noreply.github.com>
2019-08-27 00:44:21 +0530

    Add files via upload

---
+++ Assignment1Q4.java
@@ -0,0 +1,13 @@
+public class Assignment1Q6a{
+    public static void main(String[] args){
+         int a=43;
+         int b=3;
+         int c=4;
+         int d=5;
+         int x, y;
+         x=(a>b)? a:b;
+         y=(c>d)? c:d;
+         int z=(x>y)? x:y;
+         System.out.print(z);
+    }
+}
